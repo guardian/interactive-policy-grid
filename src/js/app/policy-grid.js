ccd825bfa24d788522d4594b3c3adf37396ffec4
Will Franklin <will.franklin@guardian.co.uk>
2015-03-27 00:37:19 +0000

    Removed masonry

--- src/js/app/policy-grid.js
+++ src/js/app/policy-grid.js
@@ -1,49 +1,19 @@
 define([
     'text!templates/policyGrid.html',
-    'ractive',
-    'masonry'
+    'ractive'
 ], function (
     template,
-    Ractive,
-    Masonry
+    Ractive
 ) {
-    // This hopefully won't be necessary soon
-    function masonryItemDecorator(node) {
-        var that = this;
-        return {
-            'teardown': function () {
-                that.masonry.remove(node);
-            }
-        };
-    }
-
     return Ractive.extend({
         'template': template,
         'data': {
             'policyNo': 0
         },
-        'decorators': {
-            'masonryItem': masonryItemDecorator
-        },
         'oncomplete': function () {
-            this.masonry = new Masonry(this.find('.policy-grid'), {
-                'itemSelector': '.policy-grid__item',
-                'columnWidth': '.policy-grid__sizer',
-                'transitionDuration': 0
-            });
-
             this.on('policy', function (evt) {
                 evt.original.preventDefault();
                 this.set('policyNo', evt.context.rowNumber);
-                this.masonry.layout();
-            });
-
-            this.observe('policies', function () {
-                this.masonry.reloadItems();
-                this.masonry.layout();
-            }, {
-                'init': false,
-                'defer': true
             });
         }
     });
