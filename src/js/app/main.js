9e74599321c59f0b9e945c22804ede8ceb6e4d34
Will Franklin <will.franklin@guardian.co.uk>
2015-04-09 12:01:58 +0100

    Initial look at packages to see how they might fit together

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -13,12 +13,35 @@ define([
             .filter(function (item) { return item.length > 0; });
     }
 
+    function c() { return Math.floor(Math.random() * 256); }
+
     function init(el) {
         pegasus(SHEET_URL).then(function (spreadsheet) {
+            var packages = {};
+            spreadsheet.sheets.packages.forEach(function (pkg) {
+                packages[pkg.id] = {
+                    'name': pkg.name,
+                    'color': 'rgba(' + c() + ',' + c() + ',' + c() + ', 0.5)'
+                };
+            });
+
             var policies = spreadsheet.sheets.policies.map(function (policy) {
                 policy.area = policy.area.toLowerCase();
                 policy.answers = parseList(policy.answers);
+                policy.pkg = packages[policy.package];
                 return policy;
+            });
+
+            policies.sort(function (a, b) {
+                if (a.pkg && b.pkg) {
+                    return a.pkg.name === b.pkg.name ? 0 : (a.pkg.name > b.pkg.name ? 1 : -1);
+                } else if (a.pkg) {
+                    return -1;
+                } else if (b.pkg) {
+                    return 1;
+                } else {
+                    return 0;
+                }
             });
 
             var areas = spreadsheet.sheets.areas.map(function (area) {
